eae54e9f56392e2d45d2d1ff9756161ea662516f
Sherman Rofeman <skippyr.developer@gmail.com>
2023-08-03 14:59:57 -0300

    ~% add header

--- script.js
+++ script.js
@@ -12,6 +12,12 @@ function Create_Recipe(properties)
 	recipe_template.querySelector(".Recipe_Name").innerHTML = properties.name;
 	recipe_template.querySelector(".Recipe_Author").innerHTML =
 		`Written by @${properties.author}.`;
+	if (properties.can_edit) {
+		const element = document.createElement("button");
+		element.classList.add("Recipe_Button", "fa-solid", "fa-pencil");
+		element.title = "Edit Your Recipe";
+		recipe_template.querySelector(".Recipe_Buttons").appendChild(element);
+	}
 	Append_Childs(
 		properties.categories.map((category) => {
 			const element = document.createElement("li");
@@ -29,18 +35,21 @@ const recipes = [
 		name: "Noodle And Shrimp",
 		categories: ["Pasta", "Sea Food", "Vegetables"],
 		author: "juliana_pereira",
+		can_edit: true,
 		image: "assets/noodle-and-shrimp.webp"
 	}),
 	Create_Recipe({
 		name: "Shrimp Soup",
 		categories: ["Pasta", "Sea Food"],
 		author: "mauro_santos",
+		can_edit: false,
 		image: "assets/shrimp-soup.webp"
 	}),
 	Create_Recipe({
 		name: "Pepperoni Lasagna",
 		categories: ["Pasta", "Meat"],
 		author: "marcus_silva",
+		can_edit: false,
 		image: "assets/lasagna.webp"
 	})
 ];
